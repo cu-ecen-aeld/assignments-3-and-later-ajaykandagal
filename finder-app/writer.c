3388a8a186204a66968a9b7504cb0ae2f3262f4c
Ajay Kandagal <ajka9053@colorado.edu>
2023-01-29 01:04:48 -0700

    All tests passed with full-test.sh - Added sys logging in writer.c - Added rules in Makefile to compile writer.c - Modified finder-test.sh to test for writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <syslog.h>
 
 void print_usage()
 {
-    printf("Total number of arguements should be 2\n");
-    printf("The order of arguements should be:\n");
-    printf("\t1) File directory path\n");
-    printf("\t2) String to be written in to the specified file directory path\n");
+    syslog(LOG_DEBUG, "Total number of arguements should be 2");
+    syslog(LOG_DEBUG, "The order of arguements should be:");
+    syslog(LOG_DEBUG, "  1) File directory path");
+    syslog(LOG_DEBUG, "  2) String to be written in to the specified file directory path");
 }
 
 int main(int argc, char** argv)
 {
+    openlog(NULL, 0, LOG_USER);
     if (argc != 3) {
-        printf("Error: Invalid number of arguements\n");
+        syslog(LOG_ERR, "Invalid number of arguements: %d", (argc - 1));
         print_usage();
         return 1;
     }
@@ -24,14 +26,16 @@ int main(int argc, char** argv)
     fd = fopen(writefile, "w");
 
     if (fd == NULL) {
-        printf("Error: Failed to create \"%s\" file\n", writefile);
+        syslog(LOG_ERR, "Failed to open %s", writefile);
         return 1;
     }
+
+    syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
 
     int written_len = fprintf(fd, "%s", writestr);
 
     if (written_len != strlen(writestr))
-        printf("Error: Failed to write \"%s\" string into \"%s\" file\n", writestr, writefile);
+        syslog(LOG_ERR, "Failed to write %s to %s", writestr, writefile);
     
     fclose(fd);
 
