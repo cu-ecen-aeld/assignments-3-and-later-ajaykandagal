754bb13fa301f7292209c85742ec7f9f7b4b79c9
Ajay Kandagal <ajka9053@colorado.edu>
2023-02-25 00:33:00 +0530

    Added dynamic allocation through malloc while reading and writing on the socket

--- socket/aesdsocket.c
+++ socket/aesdsocket.c
@@ -1,114 +1,264 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-#define SERVER_PORT     (9000)
-#define MAX_BACKLOGS    (3)
-#define BUFFER_MAX_SIZE (20000)
+#define SERVER_PORT (9000)
+#define MAX_BACKLOGS (3)
+#define BUFFER_MAX_SIZE (1024)
+#define SOCK_DATA_FILE ("/var/tmp/aesdsocketdata")
 
-void print_buffer(char *buffer, int len);
+int process_read_data(char *buffer, int buff_len, int file_fd);
+int get_write_data(char **malloc_buffer, int total_size, int file_fd);
 
 int main()
 {
-    int server_fd;
-    int client_fd;
-    FILE *file_ptr;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len;
-    char buffer[BUFFER_MAX_SIZE];
-    int buffer_len;
     int opt = 1;
+    int ret_status = 0;
 
-    file_ptr = fopen("/var/tmp/aesdsocketdata", "w+");
+    openlog(NULL, 0, LOG_USER);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    // file_ptr = fopen("/var/tmp/aesdsocketdata", "w+");
+    int file_fd = open(SOCK_DATA_FILE, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
 
+    if (file_fd < 0)
+    {
+        printf("Failed to open %s file\n", SOCK_DATA_FILE);
+        ret_status = -1;
+        goto exit_main;
+    }
+
+    // Create server socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Check if socket is created successfully
     if (server_fd < 0)
     {
         printf("Failed to create socket\n");
-        return -1;
+        ret_status = -1;
+        goto exit_main;
     }
     else
     {
         printf("Created scoket!\n");
     }
 
+    // Set socket options for reusing address and port
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
-        printf("Failed to set socket options");
-        return -1;
+        printf("Failed to set socket options\n");
+        ret_status = -1;
+        goto exit_main;
     }
     else
     {
         printf("Successfully set socket options!\n");
     }
 
+    struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(SERVER_PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)))
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
     {
         printf("Failed to bind\n");
-        return -1;
+        ret_status = -1;
+        goto exit_main;
     }
     else
     {
         printf("Successfully binded!\n");
     }
 
-    for (int i = 0; i < 5; i++) {
+    int client_fd;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len;
+    char buffer[BUFFER_MAX_SIZE];
+    char client_addr_str[INET_ADDRSTRLEN];
+    char *write_malloc_buffer = NULL;
+    int buffer_len = 0; //, malloc_buffer_len = 0;
+    int total_recv_bytes = 0;
+
+    int count = 5;
+
+    while (count)
+    {
         if (listen(server_fd, MAX_BACKLOGS))
         {
             printf("Failed to listen\n");
-            return -1;
+            ret_status = -1;
+            goto exit_main;
         }
         else
         {
             printf("listening...\n");
         }
 
-        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+        // Accept the incoming connection
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+        // Get ip address of client in string
+        inet_ntop(AF_INET, &client_addr.sin_addr, client_addr_str, sizeof(client_addr_str));
 
         if (client_fd < 0)
         {
             printf("Failed to connect to client\n");
-            return -1;
+            ret_status = -1;
+            goto exit_main;
         }
         else
         {
+            syslog(LOG_INFO, "Accepted connection from %s", client_addr_str);
             printf("Connected to client!\n");
         }
 
-        buffer_len = read(client_fd, buffer, BUFFER_MAX_SIZE);
-        print_buffer(buffer, buffer_len);
-        fwrite(buffer, buffer_len, 1, file_ptr);
-        
-        buffer_len = 0;
-        fseek(file_ptr, 0, SEEK_SET);
-        while(fread(&buffer[buffer_len++], 1, 1, file_ptr) > 0);
-        buffer_len = write(client_fd, buffer, buffer_len - 1);
+        while (1)
+        {
 
+            while (1)
+            {
+                buffer_len = read(client_fd, buffer, BUFFER_MAX_SIZE);
+
+                if (buffer_len == 0)
+                {
+                    printf("Connection lost with client\n");
+                    break;
+                }
+                else if (buffer_len < 0)
+                {
+                    printf("Error getting data from client\n");
+                    break;
+                }
+
+                int ret_data = process_read_data(buffer, buffer_len, file_fd);
+
+                if (ret_data == -1)
+                {
+                    printf("Malloc error\n");
+                    break;
+                }
+                else if (ret_data > 0)
+                {
+                    total_recv_bytes += ret_data;
+                    break;
+                }
+                else
+                    ;
+            }
+
+            if (buffer_len <= 0)
+                break;
+
+            int ret_data = get_write_data(&write_malloc_buffer, total_recv_bytes, file_fd);
+
+            if (ret_data == -1)
+            {
+                printf("Malloc error\n");
+                break;
+            }
+            else if (ret_data != total_recv_bytes)
+            {
+                printf("Failed to read all the data from the file\n");
+            }
+            else
+            {
+                printf("Successfully read all bytes from the file\n");
+            }
+
+            buffer_len = write(client_fd, write_malloc_buffer, total_recv_bytes);
+
+            if (buffer_len == total_recv_bytes)
+            {
+                printf("Sent all bytes to client!\n");
+            }
+            else
+            {
+                printf("Error while sending data to client\n");
+                break;
+            }
+        }
+
+        syslog(LOG_INFO, "Closed connection from %s", client_addr_str);
         close(client_fd);
+        count--;
     }
-    fclose(file_ptr);
-    shutdown(server_fd, SHUT_RDWR);
-    
+
+exit_main:
+    if (write_malloc_buffer)
+        free(write_malloc_buffer);
+
+    if (file_fd > 0)
+        close(file_fd);
+
+    if (server_fd > 0)
+        shutdown(server_fd, SHUT_RDWR);
+
+    if (remove(SOCK_DATA_FILE) < 0)
+        printf("Error while deleting %s file\n", SOCK_DATA_FILE);
+
+    return ret_status;
+}
+
+int process_read_data(char *buffer, int buff_len, int file_fd)
+{
+    static char *malloc_buffer = NULL;
+    static int malloc_buffer_len = 0;
+    int index = 0;
+
+    // Find the delimeter \n in the received buffer
+    for (index = 0; index < buff_len && buffer[index] != '\n'; index++)
+        ;
+
+    if (malloc_buffer == NULL)
+        malloc_buffer = (char *)malloc(sizeof(char) * (index == buff_len ? buff_len : index + 1));
+    else
+        malloc_buffer = (char *)realloc(malloc_buffer, (malloc_buffer_len + (index == buff_len ? buff_len : index + 1)));
+
+    if (malloc_buffer == NULL)
+    {
+        return -1;
+    }
+
+    // copy data including \n and update malloc buffer length
+    memcpy(malloc_buffer + malloc_buffer_len, buffer, (index == buff_len ? buff_len : index + 1));
+    malloc_buffer_len += (index == buff_len ? buff_len : index + 1);
+
+    int ret_len = malloc_buffer_len;
+
+    if (index < buff_len)
+    {
+        write(file_fd, malloc_buffer, malloc_buffer_len);
+        free(malloc_buffer);
+        malloc_buffer = NULL;
+        malloc_buffer_len = 0;
+
+        return ret_len;
+    }
+
     return 0;
 }
 
-void print_buffer(char *buffer, int len)
+int get_write_data(char **malloc_buffer, int total_size, int file_fd)
 {
-    printf("%d : ", len);
+    if (*malloc_buffer != NULL)
+        free(*malloc_buffer);
 
-    for (int i = 0; i < len; i++)
+    *malloc_buffer = (char *)malloc(sizeof(char) * total_size);
+
+    if (*malloc_buffer == NULL)
     {
-        printf("%c", buffer[i]);
+        return -1;
     }
+
+    lseek(file_fd, 0, SEEK_SET);
+
+    int read_ret = read(file_fd, *malloc_buffer, total_size);
+
+    return read_ret;
 }
